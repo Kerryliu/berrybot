09d2a2529ce11e05254720339fe88dd53be86c67
Kerry Liu <kerry_liu@outlook.com>
2015-12-03 22:36:31 -0500

    FUCK

--- berrybot.js
+++ berrybot.js
@@ -55,7 +55,7 @@ mybot.on("message", function(message){
 				mybot.reply(message, "pong");
 				break;
 			default:
-				if (command.include("(╯°□°）╯︵ ┻━┻") {
+				if (command.include("(╯°□°）╯︵ ┻━┻")) {
 					mybot.sendMessage(message.channel, "┬──┬◡ﾉ(° -°ﾉ) ");
 				} else if(command.substr(0,3) == "ayy") {
 					mybot.reply(message, "lmao");
