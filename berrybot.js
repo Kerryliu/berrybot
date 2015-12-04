72a60ef38651b390648f83343ca33a4970002896
Kerry Liu <kerry_liu@outlook.com>
2015-12-03 20:34:18 -0500

    nicer code

--- berrybot.js
+++ berrybot.js
@@ -4,89 +4,73 @@ var request = require("request");
 var Cleverbot = require("cleverbot-node");
 var auth = require("./auth.json");
 
+var command;
+var result;
+var counter = 0;
+
 cleverbot = new Cleverbot;
 
 mybot.login(auth.email, auth.pass);
+
 mybot.on("message", function(message){
-	var command = message.content.toLowerCase();
-	var DoTheMagic = "Yes!";
-
-	if(message.content.indexOf("ding") >= 0) {
-		mybot.reply(message, "dong");
-
-	} else if (message.content.charAt(0) == '/') {
-		mybot.startTyping(message.channel);
-		console.log(command);
-
-		switch(command) {
+	command = message.content.toLowerCase();
+	switch(command) {
 		case "/help":
-			var arg = "ew";
 			mybot.reply(message, "https://github.com/Kerryliu/berrybot");
-		break;
+			break;
 		case "/pussy":
 		case "/cat":
 		case "/kitty":
-			var arg = "http://random.cat/meow";
-		break;
-		case "/e621":
-			var arg = "https://e621.net/post/index.json?limit=1&page=1&tags=order:random";
-		break;
-		case "/yandere":
-			var arg = "https://yande.re/post/index.json?limit=1&page=1&tags=order:random";
-		break;
-		case "/konachan":
-			var arg = "http://konachan.com/post/index.json?limit=1&page=1&tags=order:random";
-		break;
+			request("http://random.cat/meow", function(error, response, body) {
+				if(!error && response.statusCode == 200) {
+					result = JSON.parse(body);
+					mybot.reply(message, result.file);
+				}
+			});
+			break;
 		case "/insult":
-			var arg = "http://quandyfactory.com/insult/json";
-		break;
+			request("http://quandyfactory.com/insult/json", function(error, response, body) {
+				if(!error && response.statusCode == 200) {
+					result = JSON.parse(body);
+					mybot.reply(message, result.insult);
+				}
+			});
+			break;
 		case "/joke":
-			var arg = "http://tambal.azurewebsites.net/joke/random";
-		break;
+			request("http://tambal.azurewebsites.net/joke/random", function(error, response, body) {
+				if(!error && response.statusCode == 200) {
+					result = JSON.parse(body);
+					mybot.reply(message, result.joke);
+				}
+			});
+			break;
+		case "ping":
+			mybot.reply(message, "pong");
+			break;
+		case "(╯°□°）╯︵ ┻━┻":
+			mybot.sendMessage(message.channel, "┬──┬◡ﾉ(° -°ﾉ) ");
+			break;
 		default:
-			DoTheMagic = "Nu~";
-		}
-
-		if (DoTheMagic == "Yes!") {
-			request(arg, function(error, response, body) {
-				if (!error && response.statusCode == 200) {
-					var result = JSON.parse(body);
-					console.log(result);
-					if (command == "/pussy" || command == "/cat" || command == "/kitty") {
-						mybot.reply(message, result.file);
-					} else if (command == "/e621" || command == "/yandere" || command == "/konachan") {
-						mybot.reply(message, result[0].file_url + " Score " + result[0].score);
-					} else if (command == "/insult") {
-						mybot.reply(message, result.insult);
-					} else if (command == "/joke") {
-						mybot.reply(message, result.joke);
-					}
-				};
-			});
-
-		} else if (DoTheMagic == 'Nu~') {
-			console.log("Potato");
-			mybot.reply(message, "Invalid Parameter.  /help for list of arguments");
-		}
-		mybot.stopTyping(message.channel);
-
-	} else if (message.content.includes("(╯°□°）╯︵ ┻━┻")) {
-		console.log("Fix Table");
-		mybot.sendMessage(message.channel, "┬──┬◡ﾉ(° -°ﾉ) ");
-
-	} else if (message.content.search(/fuck|hell|shit|damn|bitch|ass|cunt/) >= 0) {
-		console.log("What a terribly awful thing to say.");
-		mybot.sendMessage(message.channel, "Watch your language now.");
-
-	} else if (message.content.includes("121398650738835458")) {
-		mybot.startTyping(message.channel);
-		Cleverbot.prepare(function(){
-			console.log(message.content);
-			cleverbot.write(message.content, function (response) {
-				console.log(response.message);
-				mybot.reply(message, response.message, true);
-				mybot.stopTyping(message.channel);
-			});
-		});
+			console.log(command);
+			if(command.substr(0,3) == "ayy") {
+				mybot.reply(message, "lmao");
+			} else if (command.substr(0,3) == "and") {
+				counter++;
+				console.log(counter);
+				if(counter > 1) {
+					mybot.reply(message, "And my axe!");
+					counter = 0;
+				}
+			} else if (command.includes("121398650738835458")) {
+				mybot.startTyping(message.channel);
+				Cleverbot.prepare(function(){
+					cleverbot.write(message.content, function (response) {
+						console.log(response.message);
+						mybot.reply(message, response.message, true);
+						mybot.stopTyping(message.channel);
+					});
+				});
+			}
+			break;
 	}
 });
